687cd63e202001135c80446fa5c071c3365922ee
Andreas Weber <info@weberandreas.eu>
2019-10-01 14:56:51 +0000

    Test database connection in python

--- model/test/test_hello.py
+++ model/test/test_hello.py
@@ -1,7 +1,18 @@
 import unittest
 from hello import say_hello
+from influxdb import InfluxDBClient
+
 
 class TestHello(unittest.TestCase):
 
     def test_say_hello(self):
         self.assertEqual(say_hello('VS Code'), 'Hello VS Code')
+
+    def test_conntection_to_influx_db(self):
+        client = InfluxDBClient('influxdb.weberandreas.eu',
+                                ssl=True,
+                                port=443,
+                                username='admin',
+                                password='css-secret-password',
+                                database='css')
+        client.ping()
