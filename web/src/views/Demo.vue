aae511adabf2f9f53f7a374c7e58f1b764f5a0f1
Andreas Weber <info@weberandreas.eu>
2019-10-02 11:03:12 +0000

    Use model in browser

--- web/src/views/Demo.vue
+++ web/src/views/Demo.vue
@@ -14,45 +14,64 @@
 
     <p>Move your smartphone to influce the music.</p>
 
-    <p>Current context: {{ mood }}</p>
+    <p>Current activity: {{ activity }}</p>
 
     <button v-on:click="playSong()">Play song</button>
     <button v-on:click="stopSong()">Stop song</button>
 
+    <ul class="sensor-values">
+      <li>Alpha: {{ alpha }}</li>
+      <li>Beta: {{ beta }}</li>
+      <li>Gamma: {{ gamma }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Orientation from '../services/orientation'
+// import {DecisionTreeClassifier} from '../../static/model.js'
 
-var song = new Audio('https://www.bensound.org/bensound-music/bensound-thejazzpiano.mp3')
-song.play()
+// var song = new Audio('https://www.bensound.org/bensound-music/bensound-thejazzpiano.mp3')
+// song.play()
 export default {
-  methods: {
-    playSong: function () {
-      song.play()
-    },
-    stopSong: function () {
-      song.pause()
-      song.currentTime = 0
+  data: function () {
+    return {
+      activity: 'testing',
+      alpha: 0,
+      beta: 0,
+      gamma: 0
     }
+  },
+  methods: {
+    // playSong: function () {
+    //   song.play()
+    // },
+    // stopSong: function () {
+    //   song.pause()
+    //   song.currentTime = 0
+    // },
+    predictActivity: function () {
+      const features = this.orientation.mean()
+      this.orientation.reset()
+      this.activity = this.classifier.predict(features)
+    }
+  },
+  computed: {
+    sensorSupport: function () {
+      return window.DeviceOrientationEvent
+    }
+  },
+  created: function () {
+    this.orientation = new Orientation()
+    this.classifier = new DecisionTreeClassifier()
+    setInterval(this.predictActivity, 1000 / 20)
+    window.addEventListener('deviceorientation', (event) => {
+      this.alpha = event.alpha
+      this.beta = event.beta
+      this.gamma = event.gamma
+      this.orientation.push(event)
+    })
   }
 }
-
-if (typeof process !== 'undefined' && typeof process.argv !== 'undefined') {
-    if (process.argv.length - 2 === 3) {
-
-        // Features:
-        var features = process.argv.slice(2);
-
-        // Prediction:
-        var clf = new DecisionTreeClassifier();
-        var prediction = clf.predict(features);
-        console.log(prediction);
-
-    }
-}
-
 // var song = new Audio('https://www.bensound.org/bensound-music/bensound-happyrock.mp3')
-song.play()
-
 </script>
